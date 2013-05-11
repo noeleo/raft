62248ed2dd1a2142760067ad2f39e9f5a5a35355
Noel Moldvai <noelmoldvai@gmail.com>
2013-05-11 01:59:45 -0700

    fixed timer?

--- progress_timer.rb
+++ progress_timer.rb
@@ -2,6 +2,11 @@ require 'rubygems'
 require 'bud'
 require 'time'
 
+# Progress Timer works by setting an alarm with a timeout via the set_alarm input 
+# interface and having it go off via the alarm output. If another set_alarm is
+# issued before the current alarm goes off, the alarm will be reset. There is only
+# a single timer.
+
 module ProgressTimerProtocol
   state do
     interface :input, :set_alarm, [] => [:time_out]
@@ -15,14 +20,16 @@ module ProgressTimer
   state do
     table :timer_state, [] => [:start_tm, :time_out]
     table :buffer, set_alarm.schema
-    scratch :cyc, timer_state.schema
+    scratch :cyc, [:start_tm, :time_out]
+    scratch :single_cyc, cyc.schema
     periodic :timer, 0.001
   end
 
   bloom do
     buffer <= set_alarm
     cyc <= (buffer * timer).pairs {|b, t| [t.val.to_f, b.time_out]}
-    timer_state <+- cyc
+    single_cyc <= cyc.argagg(:max, [], :start_tm)
+    timer_state <+- single_cyc {|c| [c.start_tm, c.time_out]}
     buffer <- cyc {|c| [c.time_out]}
 
     alarm <= (timer_state * timer).pairs do |s, t|
